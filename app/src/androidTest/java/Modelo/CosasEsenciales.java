f00ee1361ee05a2e6dbc11e373c95a7eff1c8e8d
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-07-22 23:53:15 -0600

    Creacion de modelos que recibiran las clases

---
+++ app/src/androidTest/java/Modelo/CosasEsenciales.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class CosasEsenciales {
+    private String nombre;
+    private String url;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/androidTest/java/Modelo/RespuestasApiVideoJuegos.java
@@ -0,0 +1,15 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class RespuestasApiVideoJuegos {
+    private ArrayList <CosasEsenciales> result;
+
+    public ArrayList<CosasEsenciales> getResult() {
+        return result;
+    }
+
+    public void setResult(ArrayList<CosasEsenciales> result) {
+        this.result = result;
+    }
+}
